ab282d7060365bcb9c9482d06285d9f41c6b9e50
Archiroo <newbieit.nb@gmail.com>
2021-12-18 22:01:06 +0700

    css tạm

--- index.php
+++ index.php
@@ -5,11 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.scss">
     <title>Gladys Food Website</title>
 </head>
 <body>
-        
+    <header class="header">
+        <!-- Logo -->
+    <a href="index.php" class="logo"><i class="fas fa-utensils"></i>Gladys</a>
+
+    <!-- Thẻ điều hướng -->
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#popular">popular</a>
+        <a href="#menu">menu</a>
+        <a href="#order">order</a>
+        <a href="#blogs">blogs</a>
+    </nav>
+
+    <!-- Các icon -->
+    <div class="icons">
+        <div id="menu-btn" class="fas fa-bars"></div>
+        <div id="search-btn" class="fas fa-search"></div>
+        <div id="cart-btn" class="fas fa-shopping-cart"></div>
+        <div id="login-btn" class="fas fa-user"></div>
+    </div>
+
+    </header>    
 
 
 
